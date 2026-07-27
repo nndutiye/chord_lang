b6ca944b1694393678c6cbc2b90d433779288fd4
nndutiye <nndutiye@techfak.uni-bielefeld.de>
2026-07-27 22:45:27 +0200

    first changes to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
@@ -145,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Select a scale:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
