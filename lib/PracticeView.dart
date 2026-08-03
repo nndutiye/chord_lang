9bf27314e0cf424f87cc8e6338079627b879967d
nndutiye <nndutiye@techfak.uni-bielefeld.de>
2026-08-04 00:46:06 +0200

    subscribing and cancelling it is working properly

--- lib/PracticeView.dart
+++ lib/PracticeView.dart
@@ -1,12 +1,17 @@
+import 'dart:async';
+
+import 'package:chord_lang/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_midi_command/flutter_midi_command.dart';
 
 class Practiceview extends StatelessWidget {
   const Practiceview({super.key});
+  
 
-  void connectToMidiDevice() async {
+  Future<StreamSubscription<MidiDataReceivedEvent>> connectToMidiDevice() async {
     final midi = MidiCommand();
-    final received = <MidiDataReceivedEvent>[];
+    
+    //final received = <MidiDataReceivedEvent>[];
 
     final device = (await midi.devices)!.first;
 
@@ -15,20 +20,29 @@ class Practiceview extends StatelessWidget {
       awaitConnectionTimeout: const Duration(seconds: 1),
     );
     
-
+    if (device.connected) {
+      print("conneced!");
+    }
     
-    final sub = midi.onMidiDataReceived!.listen(
-      (data) async {
+    final sub_init = midi.onMidiDataReceived!.listen(
+      (data) {
+        //print('Here');
         String s = data.message.data[1].toString();
         //String t = data.toString();
         print('U8intlist: $s');
         //print('Just data: $t');
-      }
-    );
-      
-    //print("The data is: " + received.first.message.data.toString());
-    //await Future<void>.delayed(const Duration(seconds: 1000));
 
+      },
+    );
+    return sub_init;
+
+    //await Future<void>.delayed(const Duration(seconds: 1000));
+    //print("cancel");
+    //sub.cancel();
+  }
+
+  void cancelSubscription(Future<StreamSubscription<MidiDataReceivedEvent>> s) async {
+    StreamSubscription<MidiDataReceivedEvent> sub = await s;
     sub.cancel();
   }
 
@@ -36,9 +50,21 @@ class Practiceview extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    connectToMidiDevice();
+    Future<StreamSubscription<MidiDataReceivedEvent>> sub = connectToMidiDevice();
+    //cancelSubscription(sub);
     return Scaffold(
       appBar: AppBar(title: const Text('Practice Session')),
+      body: OutlinedButton(
+              onPressed: () {
+                cancelSubscription(sub);
+
+
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => MyApp())); // Source - https://stackoverflow.com/a/54165550 Posted by Suragch, modified by community. See post 'Timeline' for change history Rerieved 2026-07-30, License - CC BY-SA 4.0
+
+              },
+
+              child: Text('End Practice Session'),
+            ),
     );
   }
 }
