a6246a83deb6342766a8798b5da9328de6ea31a8
nndutiye <nndutiye@techfak.uni-bielefeld.de>
2026-08-13 15:59:49 +0200

    CG has half steps list

--- lib/model/ChordGenerator.dart
+++ lib/model/ChordGenerator.dart
@@ -2,6 +2,9 @@ import 'package:chord_lang/model/MidiValue.dart';
 
 class ChordGenerator {
   final String scale;
+  static const List<int> major_half_steps = [2,2,1,2,2,2,1];
+  static const List<int> minor_half_steps = [2,1,2,2,1,2,2];
+
   const ChordGenerator(this.scale);
   
   String getTonicAsMidiString() {
