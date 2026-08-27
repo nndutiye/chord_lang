ee3f49986d62ac239250a5574631e551fcee8ab7
nndutiye <nndutiye@techfak.uni-bielefeld.de>
2026-08-27 19:15:14 +0200

    added more scales to scale list

--- lib/model/Scale.dart
+++ lib/model/Scale.dart
@@ -1,5 +1,6 @@
 class Scale {
-  List<String> scales = ['C', 'G', 'F', 'D', 'A', 'E', 'B',];
+  //List<String> scales = ['C', 'G', 'F', 'D', 'A', 'E', 'B',];
+  List<String> scales = ['C', 'G', 'F', 'D', 'Bb', 'A', 'Eb', 'E', 'Ab', 'F#', 'Em', 'Bm', 'F#m', 'C#m', 'Dm', 'Gm', 'Cm', 'Fm', 'Am'];
   //List<String> scales = ['C', 'G', 'F', 'D', 'Bb', 'A', 'Eb', 'E', 'Ab', 'B', 'Cb', 'Db', 'C#', 'Gb', 'F#'];
   List<String> neutral = ['C', 'Am'];
   List<String> sharps = ['G', 'Em', 'D', 'Bm', 'A', 'F#m', 'E', 'C#m'];
@@ -7,4 +8,3 @@ class Scale {
   List<String> getAllScalesAsStringList() => this.scales;
 
 }
-
